0b760ec52397b40751fe529a02c8cbcbb8c9e397
Khushnud2005 <bayhushnud@gmail.com>
2022-08-19 08:51:31 +0500

    Added second activity. Added button in main activity

--- app/src/main/java/uz/exemple/kotlinapp/MainActivity.kt
+++ app/src/main/java/uz/exemple/kotlinapp/MainActivity.kt
@@ -1,11 +1,26 @@
 package uz.exemple.kotlinapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
+    }
+
+    fun initViews(){
+        val btnToSecond =  findViewById<Button>(R.id.btnToSecondActivity)
+        btnToSecond.setOnClickListener {
+            openSecondActivity()
+        }
+    }
+
+    fun openSecondActivity(){
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
     }
 }
